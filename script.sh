dbb222b6c6d7dae8ed64263d19cf83060876c6b4
Amine Boucenna <89776217+amineboucenna@users.noreply.github.com>
2023-06-30 11:02:44 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -142,18 +142,34 @@ if [ -n "$additional_packages" ]; then
 fi
 echo "Packages downloaded and installed successfully."
 
-
-echo "$installation_type" > installation_type.tmp
-
 # Generating fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # mounting chroot
 arch-chroot /mnt /bin/bash <<EOF
+# configuring
+sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 50/' etc/pacman.conf
+ln -sf /usr/share/zoneinfo/Africa/Algiers /etc/localtime
+hwclock --systohc
+sed -i '/^#en_US.UTF-8/s/^#//' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo KEYMAP=de-latin1 > /etc/vconsole.conf
+echo ArchLinuxPC > /etc/hostname
 
+systemctl enable NetworkManager
 
+if [ "$installation_type" = "1" ]; then
+    echo "Installing grub for EFI"
+    grub-install --target x86_64-efi --efi-directory /boot/efi/
+    grub-mkconfig -o /boot/grub/grub.cfg
+elif [ "$installation_type" = "2" ]; then
+    echo "Installing grub for MBR"
+    grub-install --target i386-pc "$disk"
+    grub-mkconfig -o /boot/grub/grub.cfg
+fi
 
-
+echo "Installation complete"
 EOF
 
 
